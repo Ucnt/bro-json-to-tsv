9605afe47f895c7581eb935405278a47bf0ec800
Ucnt <matt.svensson@gmail.com>
2019-09-09 19:00:31 -0400

    added try except to whole method

--- module/json_to_tsv.py
+++ module/json_to_tsv.py
@@ -13,19 +13,22 @@ from module.type_mapper import fields_dict
 
 
 def json_to_tsv(line, file_type):
-    # Parse the line into json
-    line_json = json.loads(line.strip())
+    try:
+        # Parse the line into json
+        line_json = json.loads(line.strip())
 
-    fields = []
-    for field in fields_dict[file_type]:
-        try:
-            if field == "ts":
-                d = datetime.datetime.strptime(line_json[field], '%Y-%m-%dT%H:%M:%S.%fZ')
-                fields.append("%s.%s" % (d.strftime("%s"), d.microsecond))
-            else:
-                fields.append(line_json[field])
-        except Exception as e:
-            fields.append("-")
+        fields = []
+        for field in fields_dict[file_type]:
+            try:
+                if field == "ts":
+                    d = datetime.datetime.strptime(line_json[field], '%Y-%m-%dT%H:%M:%S.%fZ')
+                    fields.append("%s.%s" % (d.strftime("%s"), d.microsecond))
+                else:
+                    fields.append(line_json[field])
+            except Exception as e:
+                fields.append("-")
 
-    line_tsv = "\t".join((map(str,fields)))
-    return line_tsv
+        line_tsv = "\t".join((map(str,fields)))
+        return line_tsv
+    except Exception as e:
+        print("Error parsing line %s - Error: %s" (line, str(e)))
